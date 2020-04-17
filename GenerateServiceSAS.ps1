2a66bb375bb403b926f3f1bf62bf72fed179f17d
Durga Prasad <dsaladi@barrick.com>
2020-04-16 19:59:47 -0700

    Commit

--- GenerateServiceSAS.ps1
+++ GenerateServiceSAS.ps1
@@ -1,5 +1,5 @@
 #This script is used to create Service SAS for a specific container which give external access
-# Login to Azure
+# Login to Microsoft Azure
 # Documentation : https://docs.microsoft.com/en-us/rest/api/storageservices/create-service-sas
 
 Connect-AzAccount
